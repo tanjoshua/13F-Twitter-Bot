ffa3dce0f6673f6ae4ba81bba1298182f4384bc7
tanjoshua <jtanjoshua@gmail.com>
2023-04-06 10:42:08 -0400

    increase tweet threshold

--- src/13f.js
+++ src/13f.js
@@ -17,7 +17,7 @@ const parseHoldings = async (filerId, newQ) => {
     change = Math.abs(record.change_in_shares);
     pChange = change / record.quarter_one_shares;
     if (pChange >= 0.10 
-      && (record.quarter_one_percent_of_portfolio >= 1 || record.quarter_two_percent_of_portfolio >= 1)
+      && (record.quarter_one_percent_of_portfolio >= 5 || record.quarter_two_percent_of_portfolio >= 5)
       ) {
         applicableRecords.push(record);
     }
